e30331af33c0f1b6f5334bac854c89dcce037b5d
Avichal Rakesh <avichal.rakesh@gmail.com>
2017-11-04 23:11:10 -0500

    add SMS_RECEIVED listener

---
+++ app/src/main/java/com/highonh2o/picontinuity/SMSReceiver.java
@@ -0,0 +1,37 @@
+package com.highonh2o.picontinuity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+/**
+ * Created by avichalrakesh on 11/4/17.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("Intent", "Received Intent");
+
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+
+            if (pdus != null) {
+                SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte []) pdus[i]);
+                    String address = messages[i].getOriginatingAddress();
+                    String message = messages[i].getDisplayMessageBody();
+                    Log.d("Messages", String.format("%s, %s", address, message));
+                }
+            }
+        }
+//        Intent background = new Intent(context, SMSReceiver.class);
+//        context.startService(background);
+    }
+}
