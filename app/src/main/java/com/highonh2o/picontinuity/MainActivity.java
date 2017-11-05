d1d5b43c4dcace175f8530f11663f0b922d8d398
Avichal Rakesh <avichal.rakesh@gmail.com>
2017-11-05 00:31:04 -0500

    read sent sms

--- app/src/main/java/com/highonh2o/picontinuity/MainActivity.java
+++ app/src/main/java/com/highonh2o/picontinuity/MainActivity.java
@@ -1,14 +1,41 @@
 package com.highonh2o.picontinuity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Uri uri = Uri.parse("content://sms/sent");
+        Cursor c = getContentResolver().query(uri, null, null, null, null);
+
+        if (c != null && c.moveToFirst()) {
+            int ctr = 0,
+                    target = 50;
+
+            do {
+                StringBuilder msgData = new StringBuilder();
+                for (int i = 0; i < c.getColumnCount(); i++) {
+                    msgData.append(String.format("%s : %s\n", c.getColumnName(i), c.getString(i)));
+                }
+                Log.d(TAG, msgData.toString());
+                ctr++;
+            } while (ctr < target && c.moveToNext());
+        }
+
+        if (c != null) {
+            c.close();
+        }
     }
 }
