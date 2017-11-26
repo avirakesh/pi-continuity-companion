044a2814e39d35ecaab3e1de5e7b4e8da3437542
Avichal Rakesh <avichal.rakesh@gmail.com>
2017-11-26 04:13:49 -0600

    track only the latest time of sms seen

--- app/src/main/java/com/highonh2o/picontinuity/Globals.java
+++ app/src/main/java/com/highonh2o/picontinuity/Globals.java
@@ -1,14 +1,42 @@
 package com.highonh2o.picontinuity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by avichalrakesh on 11/7/17.
  */
 
 class Globals {
-    static SMSData latestSmsSeen = null;
-
     // Hard Coded for now.
     static final String URL_BASE = "http://192.168.2.4:8088/";
     static final String ADD_SMS_EXT = "add-sms";
+
+
+
+    private static long latestSmsTime = 0;
+    private static boolean readFromPrefs = false;
+
+    public static void updateLatestSmsTime(Context context, long newLatest) {
+        String prefsName = context.getString(R.string.package_name) + "." + context.getString(R.string.preference_file_key);
+        SharedPreferences sharedPrefs = context.getSharedPreferences(prefsName, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor spEdit = sharedPrefs.edit();
+        spEdit.putLong(context.getString(R.string.last_timestamp), newLatest);
+
+        spEdit.apply();
+        readFromPrefs = true;
+    }
+
+    public static long getLatestSmsTime(Context context) {
+        if (!readFromPrefs) {
+            String prefsName = context.getString(R.string.package_name) + "." + context.getString(R.string.preference_file_key);
+            SharedPreferences sharedPrefs = context.getSharedPreferences(prefsName, Context.MODE_PRIVATE);
+            latestSmsTime = sharedPrefs.getLong(context.getString(R.string.last_timestamp), 0);
+            readFromPrefs = true;
+        }
+
+        return latestSmsTime;
+    }
 
 }
